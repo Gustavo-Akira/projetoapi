0322abaf2b0d62ff50341371d02882dc97a136ec
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2020-03-01 18:23:37 -0300

    Implements Post for tasks

--- src/main/java/com/example/demo/resources/ProjectsResources.java
+++ src/main/java/com/example/demo/resources/ProjectsResources.java
@@ -1,7 +1,9 @@
 package com.example.demo.resources;
 
 import com.example.demo.entities.Projects;
+import com.example.demo.entities.Tasks;
 import com.example.demo.services.ProjectsService;
+import com.example.demo.services.TasksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping( value="/projects")
 public class ProjectsResources {
-
+    @Autowired
+    private TasksService tasksservice;
     @Autowired
     private ProjectsService service;
 
@@ -30,4 +33,10 @@ public class ProjectsResources {
         service.Insert(project);
         return  ResponseEntity.ok().build();
     }
+    @PostMapping(value="/tasks")
+    public ResponseEntity createTask(@RequestBody Tasks tasks){
+        tasksservice.newInsert(tasks);
+        return ResponseEntity.ok().build();
+    }
+
 }
